ff073e7aaf0a883cbfe060336d892927643c9ca7
sickl8 <sicklevy@gmail.com>
2021-07-11 20:13:52 +0100

    still working properly

--- Makefile
+++ Makefile
@@ -5,5 +5,5 @@ all:
 	mkdir -p /home/$(LOGIN)/data/wp-files
 	mkdir -p /home/$(LOGIN)/data/wp-db
 	rm -rf /home/$(LOGIN)/data/wp-files/*
-	rm -rf /home/$(LOGIN)/data/wp-db/*
+	#rm -rf /home/$(LOGIN)/data/wp-db/*
 	cd srcs && docker-compose up --build
--- srcs/mariadb/entry.sh
+++ srcs/mariadb/entry.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 
-rm -rf /var/lib/mysql/*
-
 cp /50s.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
 
-/usr/bin/env
-
-mysql_install_db
-
-/usr/bin/env
-
-service mysql start
-
-sleep 3
-expect /script.exp
-
-#sh /answer.sh | mysql_secure_installation
-
-sleep 3
-/usr/bin/env
-
-
-mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
-mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "CREATE USER ${MAINDB}@'%' IDENTIFIED BY '${PASSWDDB}';"
-mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
-mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'%';"
-mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
-
-#mysqld
-
-cd '/usr' ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
-
-tail -f
+if [ ! "$(ls -A /var/lib/mysql)" ]; then
+	mysql_install_db
+	service mysql start
+	sleep 3
+	expect /script.exp
+	sleep 3
+	mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+	mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+	mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "CREATE USER ${MAINDB}@'%' IDENTIFIED BY '${PASSWDDB}';"
+	mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+	mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'%';"
+	mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+	mysql -uwordpress -p${PASSWDDB} wordpress < wp_db.sql
+	killall -9 mysqld mysqld_safe
+	cd '/usr' ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
+else
+	#service mysql start
+	#sleep 3
+	killall -9 mysqld mysqld_safe 2>/dev/null
+        cd '/usr' ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
+fi
