39b4f6851831626905163e9640999223368fae7f
sickl8 <sicklevy@gmail.com>
2021-07-11 17:55:19 +0100

    finally workinggit status!

--- srcs/mariadb/entry.sh
+++ srcs/mariadb/entry.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 rm -rf /var/lib/mysql/*
+
+cp /50s.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
 
 /usr/bin/env
 
@@ -10,17 +12,24 @@ mysql_install_db
 
 service mysql start
 
-# expect /script.exp
+sleep 3
+expect /script.exp
 
-sh /answer.sh | mysql_secure_installation
+#sh /answer.sh | mysql_secure_installation
 
+sleep 3
 /usr/bin/env
+
 
 mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "CREATE USER ${MAINDB}@'%' IDENTIFIED BY '${PASSWDDB}';"
 mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'%';"
 mysql -uroot -p${MYSQL_NEW_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
-# mysqld
+#mysqld
+
+cd '/usr' ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
 
 tail -f
